113db9e5b0460fa872268fc4287f8d55f72f869d
thespecial94 <45978401+thespecial94@users.noreply.github.com>
2023-11-06 15:37:04 +0300

    Add files via upload

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -17,25 +17,21 @@ public class CyclesTheme {
                 "] сумма четных чисел = " + sumEvenNums + ", а нечетных = " + sumOddEvenNums);
 
         System.out.println("\n2. Вывод чисел в порядке убывания");
-        int a = -1;
-        int b = 5;
-        int c = 10;
-        int max = 0;
-        int min = 0;
-        if (a > b) {
-            max = a;
-        } else {
+        int a = 5;
+        int b = 10;
+        int c = -1;
+        int max = a;
+        int min = a;
+        if (b > max) {
             max = b;
-        }
+        } 
         if (c > max) {
             max = c;
         }
-        if (a < b) {
-            min = a;
-        } else {
+        if (min > b) {
             min = b;
-        }
-        if (c < min) {
+        } 
+        if (min > c) {
             min = c;
         }
         for (int i = max - 1; i > min; i--) {
@@ -60,7 +56,7 @@ public class CyclesTheme {
         for (int i = 1; i <= finish; i += 2) {
             System.out.printf("%3d", i);
             countNumsPerLine++;
-            if (countNumsPerLine == 5){
+            if (countNumsPerLine == 5) {
                 countNumsPerLine = 0;
                 System.out.println();
             } else if (finish - i < 2) {
@@ -99,90 +95,89 @@ public class CyclesTheme {
         for (int i = 0; i <= 4; i++) {
             System.out.println("**********");
         }
-        int triangle = 5;
-        int counterTriangle = 4;
+
+        int rows = 5;
+        int columns = 4;
         System.out.println();
-        while (triangle > 0) {
+        while (rows > 0) {
             System.out.print("#");
-            triangle--;
-            if (triangle == 0) {
-                triangle += counterTriangle;
-                counterTriangle--;
+            rows--;
+            if (rows == 0) {
+                rows += columns;
+                columns--;
                 System.out.println();
             }
         }
 
-        counterTriangle = 1;
-        triangle = 1;
+        columns = 1;
+        rows = 1;
         do {
-            if (counterTriangle == 3) {
-                triangle++;
-                if (triangle == 4) {
-                    counterTriangle = 0;
+            if (columns == 3) {
+                rows++;
+                if (rows == 4) {
+                    columns = 0;
                 } else {
-                    counterTriangle = 1;
+                    columns = 1;
                 }
             }
-            if (counterTriangle == 1) {
+            if (columns == 1) {
                 System.out.print("\n$");
             } else {
-                if (triangle == 1 || triangle == 5) {
+                if (rows == 1 || rows == 5) {
                     System.out.print("");
                 } else {
                     System.out.print("$");
                 }
             }
-            counterTriangle++;
-        } while (triangle < 5);
+            columns++;
+        } while (rows < 5);
 
         System.out.println("\n\n7. Отображение ASCII-символов");
-        System.out.printf("%1s%12s%15s", "DECIMAL", "CHARACTER", "DESCRIPTION");
-        System.out.println();
+        System.out.printf("%1s%12s%15s%n", "DECIMAL", "CHARACTER", "DESCRIPTION");
         for (int i = 0; i <= 'z'; i++) {
-        boolean genuine = (i > 14 & i % 2 != 0 && i < '1') || 
-                ((i >= 'a') && (i <= 'z') && i % 2 == 0);
+            boolean genuine = (i > 14 && i < '0' && i % 2 != 0) || 
+                    (i >= 'a' && i <= 'z' && i % 2 == 0);
             if (genuine) {
-                System.out.printf("%4s%11s%13s%-1s", i, (char) i, "",Character.getName(i));
-                System.out.println();
+                System.out.printf("%4s%11c%13s%-1s%n", i, i, "",Character.getName(i));
             }
         }
 
         System.out.println("\n8. Проверка, является ли число палиндромом");
-        int reversedNum = 1234321;
-        int compareReversedNum = reversedNum;
-        int reversedTmpNum = 0;
-        while (reversedNum > 0) {
-            reversedTmpNum = (reversedTmpNum * 10) + (reversedNum % 10);
-            reversedNum /= 10;
+        srcNum = 1234321;
+        copySrcNum = srcNum;
+        int reverseSrcNum = 0;
+        while (srcNum > 0) {
+            reverseSrcNum = (reverseSrcNum * 10) + (srcNum % 10);
+            srcNum /= 10;
         }
-        if (reversedTmpNum == compareReversedNum) {
-            System.out.println("Число " + compareReversedNum + " является палиндромом");
+        if (reverseSrcNum == copySrcNum) {
+            System.out.println("Число " + copySrcNum + " является палиндромом");
         } else {
-            System.out.println("Число " + compareReversedNum + " не является палиндромом");
+            System.out.println("Число " + copySrcNum + " не является палиндромом");
         }
 
         System.out.println("\n9. Проверка, является ли число счастливым");
-        int srcNum5 = 143413;
-        int tmp4 = srcNum5 / 1000;
-        int tmp5 = srcNum5 % 1000;
-        int tmpNum4 = tmp4;
-        int tmpNum5 = tmp5;
-        int sumTmp4 = 0;
-        int sumTmp5 = 0;
-        int lengthNum = String.valueOf(srcNum5).length();
+        srcNum = 143413;
+        int firstHalfNum = srcNum / 1000;
+        int secondHalfNum = srcNum % 1000;
+        int tmpNum4 = firstHalfNum;
+        int tmpNum5 = secondHalfNum;
+        int sumFirstHalfNum = 0;
+        int sumSecondHalfNum = 0;
+        int lengthNum = String.valueOf(srcNum).length();
         for (int i = 0; i < lengthNum / 2; i++) {
-            sumTmp4 += tmp4 % 10;
-            sumTmp5 += tmp5 % 10;
-            tmp4 /= 10;
-            tmp5 /= 10;
+            sumFirstHalfNum += firstHalfNum % 10;
+            sumSecondHalfNum += secondHalfNum % 10;
+            firstHalfNum /= 10;
+            secondHalfNum /= 10;
         }
-        if (sumTmp4 == sumTmp5) {
-            System.out.println("Число " + srcNum5 + " является счастливым");
+        if (sumFirstHalfNum == sumSecondHalfNum) {
+            System.out.println("Число " + srcNum + " является счастливым");
         } else {
-            System.out.println("Число " + srcNum5 + " не является счастливым");
+            System.out.println("Число " + srcNum + " не является счастливым");
         }
-        System.out.println("Сумма цифр " + tmpNum4 + " = " + sumTmp4 + 
-                ", а сумма " + tmpNum5 + " = " + sumTmp5);
+        System.out.println("Сумма цифр " + tmpNum4 + " = " + sumFirstHalfNum + 
+                ", а сумма " + tmpNum5 + " = " + sumSecondHalfNum);
 
         System.out.println("\n10. Отображение таблицы умножения Пифагора");
         System.out.println("       Таблица   Пифагора");
